700c20422338edcf769bfb2246a1f762183b6285
Jhonatan <2530136@upv.edu.mx>
2025-10-27 09:54:59 -0600

    Mi primer Repo

---
+++ src/understanding_variables.py
@@ -0,0 +1,21 @@
+message = " This is my first python variable "
+another_message = " I am really, really, really happy "
+
+# print() -> use to show messages in terminal
+# print() -> se utiliza para mostrar mensajes en la terminal
+print(message)
+print(another_message)
+print(message, another_message, message)
+print(another_message, message)
+message = "I love Python"
+print(message)
+
+"""
+Los nombres de variables en python deben nombrarse sonlo con: 
+
+  - Letras, numero y gion bajo (espacios)
+  - Deben comenzar con una letra o un guion bajo, pero nno con un numero:
+    ejemplo correcto: messaje_1
+
+
+"""
