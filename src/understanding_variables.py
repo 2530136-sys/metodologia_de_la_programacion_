063ae86629179c41a24ae1f1adc31beb7775c108
Jhonatan <2530136@upv.edu.mx>
2025-10-29 09:41:53 -0600

    feat: traceback

--- src/understanding_variables.py
+++ src/understanding_variables.py
@@ -1,6 +1,29 @@
+message = "This is my first python variable"
+another_message = "I am really, really, really happy"
 
+# print() -> use to show
+# print() -> se utiliza para mostrar mensajes en la terminal
+print(message)
+print(another_message)
+print(message, another_message, message)
+print(another_message, message)
 
+print(message)
 
+"""
+
+Los nombres de variables en Python deben noombrarse solo con:
+    - letras, numeros y guion bajo (espacio)
+    - deben comenzar coon la letra o con un guióno bajo, pero No con número:
+        ejemplo correcto: message_1 ( :) )
+        ejemplo incorrecto: 1_message (x) 
+    - no utilizar espacios para separar palabras en los nombres de las variables
+    - no utilizar palabrass reservadas de Python para nombrar variables o archivos
+    - deben ser cortos, pero descriptivos
+    - deben ser en inglés 
+    - nombres de las variables en minúsculas        
+       
+"""
 
 jhonatan_message = "Hola soy Jhonatan y estoy aprendiendo a usar python. "
 print(jhonatan_message)
