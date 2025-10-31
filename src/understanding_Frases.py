2a7e7a939759695e63a66b73bc3e6c139160b1c0
Jhonatan <2530136@upv.edu.mx>
2025-10-31 10:32:54 -0600

    feat understandig_numbers

---
+++ src/understanding_Frases.py
@@ -0,0 +1,17 @@
+famous_person= "Lionel Messi"
+quote= "¿Que miras bobo?, andá pa' allá bobo¡"
+
+message_f_string = f"{famous_person} una vez dijo {quote}"
+print(message_f_string)
+
+famous_person= "Jorje Carvajal"
+quote= "Si no es en el monte no cuenta"
+
+message_f_string = f"{famous_person} una vez dijo {quote}"
+print(message_f_string)
+
+famous_person= "Jorje Carvajal"
+quote= "Si no es en el monte no cuenta"
+
+message_f_string = f"{famous_person} una vez dijo {quote}"
+print(message_f_string)
---
+++ src/understanding_numbers.py
@@ -0,0 +1,69 @@
+"""
+Integers - Enteros
+
+Son numeros sin punnto decimal.
+-infty , ..., -3, -2, -1, 0, 1, 2, 3, ... , infty
+
+Ejemplo:
+    
+    # Tipado dinamico
+    age = 33
+
+    
+    Los podemos sumar (+), rstar (-), multiplicar (*)y dividir (/).
+
+    potencias (**2, **3, )
+
+    Modulo (Dividido % Divisor)
+
+"""
+
+number_1 = 39
+number_2 = 13
+
+suma = number_1 + number_2
+diference = number_1 - number_2
+multiplication = number_1 * number_2
+division = number_1 / number_2
+modulo = number_1 % number_2
+power = number_1 ** 2
+
+print("Suma: ", suma)
+print("Diference: ", diference)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+print("Modulo: ", modulo)
+print("Power: ", power)
+
+
+
+print("La suma es del tipo ", type(suma))
+print("La diference es del tipo ",  type(diference))
+print("La multiplication es del tipo ", type(multiplication))
+print("La divison es del tipo ,", type(division))
+print("El modulo es del tipo ", type(modulo))
+print("La potencia es del tipo ", type(power))
+
+#Folats
+
+"""
+Folats - Reales
+
+Son numeros con punto decimal.
+Van desde -infty  infty
+
+Ejemplo:
+    
+    # Tipado dinamico
+    age = 25.5
+
+    
+    Los podemos sumar (+), rstar (-), multiplicar (*)y dividir (/).
+"""
+print(0.1 + 0.2)
+print(0.2 - 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+
+print(0.2 + 0.1)
+print(3 * 0.1)
