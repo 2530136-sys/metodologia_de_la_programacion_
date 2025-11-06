e49d06670ba1bdecc5944f8d9142a90e8061ea40
Jhonatan <2530136@upv.edu.mx>
2025-11-06 09:42:24 -0600

    feat: understandig_lists

--- src/understanding_lists.py
+++ src/understanding_lists.py
@@ -24,6 +24,7 @@ print(messages)  # Salida: Mi fruta favorita es Manzana.
 """
    Agregar elementos a una lista
     - append(): Agrega un elemento al final de la lista.
+    El metodo append() toma un solo argumento: el elemento que se desea agregar a la lista.
 """
 print("\nAgregar elementos a una lista: Metodo append()\n")
 motorcycles = ['honda', 'yamaha', 'suzuki']
@@ -34,6 +35,7 @@ print(motorcycles)
 """
     Agrega elementos a una lista
     - insert(): Agrega un elemento en una posicion especifica de la lista
+    El metodo insert() toma dos argumentos: el indice donde se desea insertar el elemento y el elemento en si.
 """
 print("\nAgregar elementos a una lista: Metodo insert()\n")
 motorcycles = ['honda', 'yamaha', 'suzuki']
@@ -42,3 +44,62 @@ motorcycles.insert(0, 'ducati')
 print(motorcycles)
 print(motorcycles[0])
 
+
+"""
+    Eliminar elementos de una lista
+    - del: Elimina un elemento de la lista en una posicion especifica de la liata.
+    La declaracion del index elimina el elemento en la posicion especificada.
+
+"""
+
+print("\nEliminar elementos de una lista: Declaracion del\n")
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+del motorcycles[0]
+print(motorcycles)
+
+
+"""
+    Eliminar elementos de una lista
+    - pop(): Elimina y devuelve el ultimo elemento de la lista.
+    El metodo pop() no requiere argumentos y elimina el ultimo elemento de la lista.
+"""
+print("\nEliminar elementos de una lista: Metodo pop()\n")
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+print(f'La motocicleta que fue eliminada es: {popped_motorcycle.upper()}')
+
+
+"""
+    Eliminar elementos de una lista
+       - pop(linex): Elimina y devuelve el elemento en la posicion especificada de la lista.
+    El metodo pop(inex) toma un argumento, que es el indice del elemento que se desea eliminar y devolver.
+"""
+print("\nEliminar elementos de una lista: Metodo pop(index)\n")
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+first_owned = motorcycles.pop(0)
+print(motorcycles)
+print(f'La primera motocicleta eliminada es: {first_owned.title()}.')
+
+"""
+   Eliminar elementos de una lista
+      - remove(): Elimina la primera aparicion de un elemento especifico en la lista.
+    El metodo remove() toma un argumento, que es el valor del elemento que se desea eliminar de la lista.
+"""
+print("\nEliminar elementos de una lista: Metodo remove()\n")
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
+motorcycles.remove('ducati')
+print(motorcycles)
+
+
+# Ejemplo practico del metodo remove()
+names = ['ana', 'luis', 'maria', 'carlos', 'maria']
+print(names)
+deleted_name = input("Ingrese el nombre que desea eliminar de la lista: ")
+names.remove(deleted_name.strip().lower())
+print(names)
